bb9868ae3f11c4884c57c954f29f5ffc505da72f
medsonmoombe <medsonmoombe21@gmail.com>
2024-03-28 10:43:30 -0700

    Add

--- devServer.js
+++ devServer.js
@@ -1,5 +1,9 @@
 const app = require("./index");
 const routes = require("./routes/router");
+const cors = require("cors");
+
+// enable cors
+app.use(cors());
 app.use("/", routes);
 app.listen(3000,function () {
       console.log("Server started. Go to http://localhost:3000/");
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const cors = require('cors');
 require('dotenv').config();
 const helmet = require("helmet");
 
 const app = express();
 
-// enable cors
-app.use(cors());
 // Middleware for file upload
 app.use(fileUpload());
 
