2f17d58e808b8c91cb23e6bdaf265638a071407f
medsonmoombe <medsonmoombe21@gmail.com>
2024-03-28 10:54:25 -0700

    reset main

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 require('dotenv').config();
 const helmet = require("helmet");
 
 const app = express();
 
+// enable cors
+app.use(cors());
 // Middleware for file upload
 app.use(fileUpload());
 
