3dc6fa73726cf4a1c0291085247c18a11431269a
medsonmoombe <medsonmoombe21@gmail.com>
2024-03-27 23:20:13 -0700

    add the secrete keys

--- services/controllers/uploadController.js
+++ services/controllers/uploadController.js
@@ -2,14 +2,20 @@ const { google } = require('googleapis');
 const fs = require('fs');
 const path = require('path');
 
+// OAuth2 credentials
+const CLIENT_ID = "582486929557-8h8hpb0ltg0l2b7auj85sdo255opalot.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN = "1//0477-c7t0kmkmCgYIARAAGAQSNwF-L9IrTscUpA0VAAJXJUyCcv1wtR05kRMNRItCeYHdZvCOGL2_HiSFxDPUQxgq697E__ztNds";
+
 // Create OAuth2 client
 const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
 );
 oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN
+  refresh_token: REFRESH_TOKEN
 });
 const drive = google.drive({ version: "v3", auth: oauth2Client });
 
