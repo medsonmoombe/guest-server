9c9d50672402c661e2f112587c030751a5a3dfc8
medsonmoombe <medsonmoombe21@gmail.com>
2024-04-10 05:59:05 -0700

    update secrete

--- routes/getAllimagesFromS3.js
+++ routes/getAllimagesFromS3.js
@@ -6,7 +6,7 @@ const file = require('express').Router();
 
 const region = process.env.AWS_REGION || 'us-east-1'
 const bucketName = process.env.AWS_BUCKET_NAME
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID || 'AKIAWJTKY7P2DZDKFXUD'
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 
--- routes/getS3buckeckUrl.js
+++ routes/getS3buckeckUrl.js
@@ -5,7 +5,7 @@ const file = require('express').Router();
 
 const region ='us-east-1'
 const bucketName = process.env.AWS_BUCKET_NAME
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID || 'AKIAWJTKY7P2DZDKFXUD'
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 
